fcfd060abf7ab40a290787e349b62643ba062dfa
heeyeon02com <heyen0205@gmail.com>
2024-12-11 16:09:22 +0900

    코스 100% 완료

--- src/main/java/store/seub2hu2/course/dto/SuccessCoursesForm.java
+++ src/main/java/store/seub2hu2/course/dto/SuccessCoursesForm.java
@@ -11,9 +11,11 @@ import lombok.ToString;
 @ToString
 public class SuccessCoursesForm {
     // 달성한 코스 목록을 표시할 때 사용하는 정보
-    private int no; // 코스 번호
-    private String name; // 코스 이름
+    private int courseNo; // 코스 번호
+    private String courseName; // 코스 이름
     private double distance; // 코스 거리
     private int level; // 코스 난이도
+    private int userNo; // 사용자 번호
+    private String nickname; // 사용자 닉네임
     private int successCount; // 각 코스 달성 횟수
 }
