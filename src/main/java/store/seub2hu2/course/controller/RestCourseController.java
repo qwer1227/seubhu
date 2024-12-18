67bd8949b93572579258fa17adfc884b04bd14f6
2eunjung <12103207@gmail.com>
2024-12-18 14:15:48 +0900

    11 ~ 12월 전체 커뮤니티 기능(이은정 작업) - 추가

---
+++ src/main/java/store/seub2hu2/course/controller/RestCourseController.java
@@ -0,0 +1,90 @@
+package store.seub2hu2.course.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import store.seub2hu2.course.dto.AddRecordForm;
+import store.seub2hu2.course.dto.AddReviewForm;
+import store.seub2hu2.course.exception.CourseReviewException;
+import store.seub2hu2.course.service.ReviewService;
+import store.seub2hu2.course.service.UserCourseService;
+import store.seub2hu2.course.vo.Records;
+import store.seub2hu2.course.vo.Review;
+import store.seub2hu2.security.user.LoginUser;
+import store.seub2hu2.security.dto.RestResponseDto;
+import store.seub2hu2.util.ListDto;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/ajax")
+public class RestCourseController {
+    @Autowired
+    private ReviewService reviewService;
+
+    @Autowired
+    private UserCourseService userCourseService;
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/addRecord")
+    @ResponseBody
+    public void addRecord(AddRecordForm form, @AuthenticationPrincipal LoginUser loginUser) {
+        // 1. 코스 완주 기록을 저장한다.
+        userCourseService.addNewRecord(form, loginUser.getNo());
+    }
+
+    @GetMapping("/reviews/{no}/{page}")
+    public ResponseEntity<RestResponseDto<ListDto<Review>>> reviews(@PathVariable("no") int courseNo,
+                                                                    @PathVariable("page") int page) {
+        // 1. Map 객체에 코스 번호, 페이지를 저장한다.
+        Map<String, Object> condition = new HashMap<>();
+        condition.put("courseNo", courseNo);
+        condition.put("page", page);
+
+        // 2. 코스에 등록된 모든 리뷰를 가져온다.
+        ListDto<Review> dto = reviewService.getReviews(condition);
+
+        // 3. 응답 데이터를 반환한다.
+        return ResponseEntity.ok(RestResponseDto.success(dto));
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/addReview")
+    @ResponseBody
+    public Review addReview(AddReviewForm form, @AuthenticationPrincipal LoginUser loginUser) {
+        // 1. 등록할 리뷰 정보를 테이블에 저장한다.
+        Review review = reviewService.addNewReview(form, loginUser.getNo());
+
+        // 2. 등록한 리뷰 정보를 반환한다.
+        return review;
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/deleteReview/{no}")
+    public ResponseEntity<RestResponseDto<String>> deleteReview(@PathVariable("no") int reviewNo,
+                                                                @AuthenticationPrincipal LoginUser loginUser) {
+        // 1. 로그인한 사용자와 리뷰 작성자가 동일하면 리뷰를 삭제한다.
+        try {
+            reviewService.deleteReview(reviewNo, loginUser.getNo());
+        } catch (CourseReviewException ex) {
+            return ResponseEntity.ok(RestResponseDto.fail(ex.getMessage()));
+        }
+
+        // 2. 응답 데이터를 반환한다.
+        return ResponseEntity.ok(RestResponseDto.success("review deleted"));
+    }
+
+    @GetMapping("/check-success/{courseNo}")
+    public ResponseEntity<RestResponseDto<String>> checkSuccess(@AuthenticationPrincipal LoginUser loginUser
+                                                                , @PathVariable("courseNo") int courseNo) {
+        // 1. 로그인한 사용자가 코스를 성공했는지 확인한다.
+        boolean isSuccess = userCourseService.checkSuccess(loginUser.getNo(), courseNo);
+        String data = isSuccess ? "success" : "fail";
+
+        // 2. 응답 데이터를 반환한다.
+        return ResponseEntity.ok(RestResponseDto.success(data));
+    }
+}
---
+++ src/main/java/store/seub2hu2/product/dto/ProdReviewDto.java
@@ -0,0 +1,40 @@
+package store.seub2hu2.product.dto;
+
+import lombok.*;
+import store.seub2hu2.product.vo.ProdReviewImg;
+
+import java.util.List;
+
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class ProdReviewDto {
+
+    // 리뷰 정보
+    private int reviewNo;
+    private String reviewTitle;
+    private String reviewContent;
+    private int rating;
+    private String reviewDate;
+
+    // 리뷰이미지
+    private List<ProdReviewImg> prodReviewImgs;
+
+    // 상품
+    private int prodNo;
+    private String prodName;
+
+    // 색상
+    private int colorNo;
+    private String colorName;
+
+    // 사이즈
+    private int sizeNo;
+    private String sizeName;
+
+    private int userNo;
+    private String userName;
+    private String userNickname;
+}
